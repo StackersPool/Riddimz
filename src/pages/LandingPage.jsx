da0f1ca3e329a88577b4d2ee56b27c2a82e6c120
cryptosmith <bollyseaglesway1@gmail.com>
2023-04-14 18:22:27 +0100

    update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import logo from './assets/img/logo.jpg'
-import './style.css'
+import image1 from './assets/images/slider-img.jpg'
+
+// Stylers
+import './assets/css/style.css'
+import './assets/css/styler.css'
+
 function LandingPage(props) {
     return (
-        <div className='head-bar'>
+        <div className='container'>
+            {/* Header */}
             <nav>
-                <h2 className='logo'>Rid<span>Dimz</span></h2>
-                {/* <img className='logo' src={logo} alt="logo" /> */}
+                {/* <h2 className='logo'>Rid<span>Dimz</span></h2> */}
+                <img className='logo' src={logo} alt="logo" />
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Service</a></li>
-                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Docs</a></li>
+                    {/* <li><a href="#">Service</a></li>
+                    <li><a href="#">Portfolio</a></li> */}
                     <li><a href="#">Contact Us</a></li>
                 </ul>
                 <button type='button '>Connect</button>
             </nav>
+            {/* Content */}<br />
+            <div className='content'>
+                <div class="row">
+                    <h1>Welcome to RID<span>DIMZ</span></h1>
+                </div>
+            </div>
+            <div className='content'>
+                <div class="row">
+                    <h4>The first web3 karaoke platform on the Stacks blockchain. 
+                    <br/><br/>Sing, socialize, and enjoy the benefits of decentralized technology. 
+                    <br/><br/>Join our community and experience the future of entertainment.
+
+                    </h4>
+                </div>
+            </div>
+            {/* Footer */}<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <footer>
+                All rights reserved. Copyright © 2023 Riddimz.
+            </footer>
         </div>
     );
 }
